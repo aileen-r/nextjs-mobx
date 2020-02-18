1f0bae3569b983be821f28998f823887af070e26
Aileen Rae <aileen@stormideas.com>
2020-02-18 14:40:01 +0000

    NextJS hello world

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function HomePage() {
+    return (
+        <h1>Hello World 👋</h1>
+    )
+//   constructor(props) {
+//     super(props);
+//     this.store = initBaseStore();
+//   }
+
+//   render() {
+//     return (
+//       <Provider BaseStore={this.store}>
+//         <FriendlyHello />
+//       </Provider>
+//     );
+//   }
+}
